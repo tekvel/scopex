19443f4c186808addb86cbe94efdb40ac50bfae9
Burtsev <Nikita.Burtsev@skoltech.ru>
2024-09-06 10:03:04 +0300

    Add copyright notice to source files

--- src/osc_panel.cpp
+++ src/osc_panel.cpp
@@ -1,3 +1,21 @@
+/*
+ * ScopeX
+ * Copyright (C) 2024 Tekvel R&D Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "osc_panel.h"
 #include <wx/wx.h>
 #include <wx/stattext.h>
@@ -27,7 +45,7 @@ OscilloscopePanel::OscilloscopePanel(wxNotebook *parent, int num_of_drawingPanel
 
     if (num_of_sinPanels == 1)
     {
-        m_text1 = new wxStaticText(m_rightPanel, wxID_ANY, _("Curents:"), wxDefaultPosition, wxDefaultSize, 0);
+        m_text1 = new wxStaticText(m_rightPanel, wxID_ANY, _("Currents:"), wxDefaultPosition, wxDefaultSize, 0);
         m_text1->Wrap( -1 );
         vbox2->Add( m_text1, 0, wxALL, 5 );
 
@@ -36,7 +54,7 @@ OscilloscopePanel::OscilloscopePanel(wxNotebook *parent, int num_of_drawingPanel
     }
     else if (num_of_sinPanels == 2)
     {   
-        m_text1 = new wxStaticText(m_rightPanel, wxID_ANY, _("Curents:"), wxDefaultPosition, wxDefaultSize, 0);
+        m_text1 = new wxStaticText(m_rightPanel, wxID_ANY, _("Currents:"), wxDefaultPosition, wxDefaultSize, 0);
         m_text1->Wrap( -1 );
         vbox2->Add( m_text1, 0, wxALL, 5 );
 
